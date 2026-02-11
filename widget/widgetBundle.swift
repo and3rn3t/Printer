3e41bbc5bbec7734d5d3187a11dc3e4e11a949be
Matthew Anderson <and3rn3t@icloud.com>
2026-02-11 14:43:59 -0600

    Add widget functionality with configuration, control, and live activity support

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Matt on 2/11/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetControl()
+        widgetLiveActivity()
+    }
+}
